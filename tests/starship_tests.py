fab41fac06200caf7f4d5292ac47142670e5d22e
Mike Combs <mike@75design.com>
2015-05-25 10:41:40 -0400

    Added setUp/tearDown to test code

--- tests/starship_tests.py
+++ tests/starship_tests.py
@@ -10,23 +10,28 @@ sys.path.append(os.path.abspath('..'))
 # import starship
 import spacewarish.starship as starship
 
+
 class Starship_tests(unittest.TestCase):
     """ Tests for the starship module"""
 
+    def setUp(self):
+        self.ship1 = starship.Ship('pirate ship0', 100, 200, 90)
+
+    def tearDown(self):
+        pass
+
     def test_init_ship(self):
-        ship1 = starship.Ship('pirate ship0', 100, 200, 90)
-        self.assertEqual(ship1.x, 100)
-        self.assertEqual(ship1.y, 200)
-        self.assertEqual(ship1.heading, 90)
-        self.assertEqual(ship1.speed, 0)
+        self.assertEqual(self.ship1.x, 100)
+        self.assertEqual(self.ship1.y, 200)
+        self.assertEqual(self.ship1.heading, 90)
+        self.assertEqual(self.ship1.speed, 0)
 
     def test_fly_ship(self):
-        ship1 = starship.Ship('pirate ship0', 100, 200, 90)
-        ship1.accelerate()
-        self.assertEqual(ship1.speed, 5)
-        ship1.move()
-        self.assertEqual(ship1.x, 105)
-        self.assertEqual(ship1.y, 200)
+        self.ship1.accelerate()
+        self.assertEqual(self.ship1.speed, 5)
+        self.ship1.move()
+        self.assertEqual(self.ship1.x, 105)
+        self.assertEqual(self.ship1.y, 200)
 
 if __name__ == '__main__':
     unittest.main()
